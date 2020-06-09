9184c010848a0465e305ff77cccd4618c1efa3fb
jsqu4re <printed.robots@gmail.com>
2020-06-09 16:57:16 +0200

    add favicons and convert script

---
+++ assets/icons/convert.bash
@@ -0,0 +1,17 @@
+convert logo2048.png -resize 36x36 android-36x36.png
+convert logo2048.png -resize 48x48 android-48x48.png
+convert logo2048.png -resize 72x72 android-72x72.png
+convert logo2048.png -resize 96x96 android-96x96.png
+convert logo2048.png -resize 144x144 android-144x144.png
+convert logo2048.png -resize 192x192 android-192x192.png
+convert logo2048.png -resize 180x180 apple-touch-icon-180x180.png
+convert logo2048.png -resize 16x16 favicon-16x16.png
+convert logo2048.png -resize 32x32 favicon-32x32.png
+convert logo2048.png -resize 265x265 favicon-265.png
+convert logo2048.png -resize 1024x1024 favicon-1024.png
+convert logo2048.png -resize 192x192 pwa-192x192.png
+convert logo2048.png -resize 512x512 pwa-512x512.png
+convert logo2048.png -resize 70x70 tile-70x70.png
+convert logo2048.png -resize 150x150 tile-150x150.png
+convert logo2048.png -resize 310x150 tile-310x150.png
+convert logo2048.png -resize 310x310 tile-310x310.png
